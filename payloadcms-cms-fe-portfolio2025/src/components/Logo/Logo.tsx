5b1cc9bc4a71134b95591969f7e2c7f383989b7d
Patrick Meaney <patrick.wm.meaney@gmail.com>
2025-03-29 10:51:10 -0600

    fixed foldername in dc dev.  changed logo to brand

--- payloadcms-cms-fe-portfolio2025/src/components/Logo/Logo.tsx
+++ payloadcms-cms-fe-portfolio2025/src/components/Logo/Logo.tsx
@@ -15,15 +15,19 @@ export const Logo = (props: Props) => {
 
   return (
     /* eslint-disable @next/next/no-img-element */
-    <img
-      alt="Payload Logo"
-      width={193}
-      height={34}
-      loading={loading}
-      fetchPriority={priority}
-      decoding="async"
-      className={clsx('max-w-[9.375rem] w-full h-[34px]', className)}
-      src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-logo-light.svg"
-    />
+    <div className="flex flex-col-mt-2 w-full">
+      <h1 className={clsx('text-[20px] text-white hover:text-green-200 font-extrabold pr-5', className)}>Patrick Meaney</h1>
+      <h2 className={clsx('text-[20px] text-gray-300 hover:text-green-200', className)}>Full Stack | DevOps | MarTech</h2>
+    </div>
+    // <img
+    //   alt="Payload Logo"
+    //   width={193}
+    //   height={34}
+    //   loading={loading}
+    //   fetchPriority={priority}
+    //   decoding="async"
+    //   className={clsx('max-w-[9.375rem] w-full h-[34px]', className)}
+    //   src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-logo-light.svg"
+    // />
   )
 }
